3e269eaba7fa18ab42f261218d6c34e432ef5344
Shift-Wace <91454544+Shift-Wace@users.noreply.github.com>
2022-06-18 12:52:50 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,11 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Shift-Wace/Wacefilterbot.git /EvaMaria
-else
+  git clone https://github.com/Shift-Wace/Wacefilterbot.git /Wacefilterbot
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /Wacefilterbot
 fi
-cd /EvaMaria
+cd /Wacefilterbot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
